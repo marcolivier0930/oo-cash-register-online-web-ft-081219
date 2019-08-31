ed474199d2c45b6ac2cd3e254e11ddd2e6e7c279
Marc Blanchard <marcblanchard0930@gmail.com>
2019-08-31 01:07:01 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,43 +1,51 @@
-
+require "pry"
 class CashRegister
 
-  attr_accessor :total, :discount, :last_transaction_amount, :items
-
-  def initialize(discount = 0)
-    @total = 0
-    @discount = discount
-    @items = []
-  end
-
-
-  def add_item(title,price,quantity = 1)
-    if quantity>1
-      i=0
-      while i<quantity
-        @items << title
-        i+=1
-      end
-    else
-      @items << title
+    attr_accessor :total, :discount, :cart, :last_transaction
+    
+    def initialize(discount = 0)
+        @total = 0
+        @discount = discount
+        @cart = []
+        @last_transaction = 0
     end
-    @total += price*quantity
-    @last_transaction_amount = @total
-    @total
-  end
 
-  def apply_discount()
-    if @discount > 0
-      @discount = @discount/100.to_f
-      @total = @total - (@total * (@discount))
-      "After the discount, the total comes to $#{@total.to_i}."
-    else
-      "There is no discount to apply."
+    def add_item(title, price, quantity = 1)
+        quantity.times do
+            @cart << title
+        end
+        # counter = 0 
+        # while counter < quantity
+        #     @cart << title
+        #     counter += 1
+        # end 
+        self.last_transaction = price * quantity
+        @total += self.last_transaction  
+    end 
+
+    def apply_discount
+        #   binding.pry
+        if self.discount == 0
+            "There is no discount to apply."
+        else
+            @total = self.total * (1-(self.discount/100.0))
+            "After the discount, the total comes to $#{@total.to_i}."
+        end
+        
     end
-  end
 
+    def items
+        #return an array of all items that have been added.
+        @cart
+    end
 
-  def void_last_transaction()
-    @total -= @last_transaction_amount
-  end
-  total 
+    def void_last_transaction
+        self.total -= self.last_transaction
+    end
 end
+
+#  cash_20= CashRegister.new(20)
+#  cash_0= CashRegister.new
+#  cash_50= CashRegister.new(50)
+
+#  binding.pry
